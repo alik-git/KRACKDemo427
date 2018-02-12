e03f5ac16c3ef5f5c007ab6b0734c3e0a4755827
Ali <ali.kuwajerwala@mail.utoronto.ca>
2018-02-12 18:58:18 -0500

    more ciphertexts added

--- aestest.py
+++ aestest.py
@@ -15,6 +15,9 @@ message2 = "Fool me twice, shame on me"
 ciphertext1 = aes1.encrypt(message1)
 ciphertext2 = aes2.encrypt(message2)
 
+dtext1 = aes2.decrypt(ciphertext1)
+dtext2 = aes2.decrypt(ciphertext2)
+
 ciphertext1 = ciphertext1.encode("hex")
 ciphertext2 = ciphertext2.encode("hex")
 
@@ -22,18 +25,28 @@ print 'Messages'
 print(message1.encode("hex"))
 print(message2.encode("hex"))
 
-print 'Cihper Text2'
+print 'Cihper Texts'
 print(ciphertext1)
 print(ciphertext2)
 
+print("Decrypted Texts")
+print(dtext1)
+print(dtext2)
+
 '''
-Cipher Texts:
+Cipher Texts(Same Key):
 c7041f44d10f1801d4e081c61dee4cbf32090d0f90f5420cc554 c7041f44d10f1801cff98bc054e21fa43b05054adff40c55c744
 
-Cipher Texts XOR:
+Cipher Texts XOR(Same Key):
 00000000000000001b190a06490c531b090c08454f014e590210
 
+Cipher Texts (Diff Key):
+c7041f44d10f1801d4e081c61dee4cbf32090d0f90f5420cc554 3315ed233f9899c261290d436107a2b73dd834517c1540044f3c
 
+Cipher Texts XOR (Diff Key):
+
+
+Messages:
 466f6f6c206d65206f6e63652c207368616d65206f6e20796f75 466f6f6c206d652074776963652c207368616d65206f6e206d65
 
 Messages XOR:
